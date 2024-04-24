8f79a7b7448916517e85c71e89509797460ed015
kjpar0317 <kjpar0317@gmail.com>
2024-04-24 21:03:13 +0900

    base64수정

--- frontend/src/util/fetch_util.ts
+++ frontend/src/util/fetch_util.ts
@@ -1,11 +1,11 @@
-import { encode } from "base-64";
+import { encode } from 'js-base64';
 
 // 이것 저것 해보자..
 export async function getTabclouditFetch(url: string) {
   return fetch(url, {
     method: "GET",
     headers: {
-      Authorization: `Bearer erera22222.eyJpc3MiOiJjbXAtc2VydmljZS1hdXRoIiwic3ViIjoic3VwZXJhZG1pbiIsImF1ZCI6ImNtcC1jbGllbnQtZnJvbnRlbmQiLCJleHAiOjE3MzkzMzc3NTUsImlhdCI6MTcwNzgwMTc1Ner33V67O07ZmU67aA7IScIiwiaXAiOiIxMjcuMC4wLjEiLCJfasdfdsfa23UiOiJsb2NhbCIsImFkbWluIjp0cnVlLCJpc1NTT0xvZ2luIjpmYWxzZSwibG9naW5Vc2VyTmFtZSI6Iuq0gOumrOyekCIsImlzT1NNYW5hZ2VyIjp0cnVlLCJsb2dpblRpbWUiOjE3MDc4MDE2ODMwMDAsImVudENvZGUiOiJEQUUiLCJ1bml0QasdfasdfsdfsdfSIsInVzZXJuYW1lIjoic3VwZXJhZG1pbiJ9fQ.pS2QIGU8roPtXHjGEeIr4z_Irdsf3333uT2fe-IfEWg`,
+      Authorization: `Bearer setaete42eemF1ZCI6ImNtcC1jbGllbesttetzgwMTc1NSwiaW5mb3MiOnsiZW50TrtIsInVuaXROYW1lIjoi7KCV67O07ZmU67aA7IScIiwiaXAiOiIxMjcuMC4wLjEiLCJjb250cmFjdCI6IjAxMC0xMjM0LTk5OTkiLCJhY3RpdmUiOiJsb2NhbCIsImFkbWluIjp0cnVlLCJpc1NTT0xvZ2luIjpmYWxzZSwibG9naW5Vc2VyTmFtZSI6Iuq0gOumrOyekCIsImlzT1NNYW5hZ2VyIjp0cnVlLCJsb2dpblRpbWUiOjE3MDc4MDE2ODMwMDAsImVudENvZGUiOiJEQUUiLCJ1bml0Q29kZSI6IjAwMiIsImVtYWlsIjoiYWJjQGlubm9ncmlkLmNvbSIsInVzZXJuYW1lIjoic3VwZXJhZG1pbiJ9fQ.pS2QIGU8roPtXHjGEeIr4z_Ir6euVqMYNz6OBJ8TsjViqzR406DEqZKvNI0a9XQzLXwKrJDJS73uT2fe-IfEWg`,
       "Content-Type": "application/json",
     },
   })
